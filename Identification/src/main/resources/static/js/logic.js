db42042ad080d8d3850dc7a85336400ec40b0d45
icrovettM <55193000+icrovettM@users.noreply.github.com>
2019-09-25 16:35:46 +0200

    generado a partir de un Digital Twin
    Prueba de generar el codigo de un DT en maven y compilar OK.

---
+++ Identification/src/main/resources/static/js/logic.js
@@ -0,0 +1,6 @@
+var digitalTwinApi = Java.type('com.minsait.onesait.platform.digitaltwin.logic.api.DigitalTwinApi').getInstance();
+function init(){}
+function main(){}
+var onActionAvisoTemperaturaAlta=function(data){ }
+var onActionAvisoTemperaturaAlta=function(data){ }
+var onActionApagar=function(data){ }
