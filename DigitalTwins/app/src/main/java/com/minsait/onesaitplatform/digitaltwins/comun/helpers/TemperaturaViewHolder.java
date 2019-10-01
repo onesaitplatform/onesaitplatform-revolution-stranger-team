c9cdc6e63ec87cdda89830c202e9d33c602adb4d
maruizi <maruizi@minsait.com>
2019-10-01 22:20:31 +0200

    Primera subida, queda pendiente una limpieza de código.

---
+++ DigitalTwins/app/src/main/java/com/minsait/onesaitplatform/digitaltwins/comun/helpers/TemperaturaViewHolder.java
@@ -0,0 +1,34 @@
+package com.minsait.onesaitplatform.digitaltwins.comun.helpers;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.minsait.onesaitplatform.digitaltwins.R;
+
+public class TemperaturaViewHolder extends RecyclerView.ViewHolder {
+    public String mBoundString;
+
+    public final View mView;
+    public final ImageView mImageView;
+    public final TextView mTextViewFecha;
+    public final TextView mTextViewGrados;
+
+    public TemperaturaViewHolder(View view) {
+        super(view);
+        mView = view;
+        mImageView = view.findViewById(R.id.img_temperatura);
+        mTextViewFecha = view.findViewById(R.id.txt_fecha);
+        mTextViewGrados = view.findViewById(R.id.txt_grados);
+    }
+
+    public void clearAnimation() {
+        mView.clearAnimation();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " " + mTextViewFecha.getText();
+    }
+}
